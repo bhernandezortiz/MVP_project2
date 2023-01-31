d9d8fd6ed167e98db2e81cd4ac41ec95dadc7093
bhernandezortiz <neybrian11@gmail.com>
2023-01-31 09:00:19 -0800

    remote database complete

---
+++ server.js
@@ -0,0 +1,15 @@
+dotenv.config()
+
+import express from "express"
+import postgres from "postgres"
+import dotenv from "dotenv"
+
+const app = express();
+
+const sql = postgres(process.env.DATABASE_URL);
+
+app.use(express.static("public"));
+
+app.listen(process.env.PORT, () => {
+    console.log(`listening on Port ${process.env.PORT}`);
+});
